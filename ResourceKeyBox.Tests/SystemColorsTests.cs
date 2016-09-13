2ebef71b1fe30099c5bdceefa13d2613c96fc4bf
Johan Larsson <johan_gavle1@hotmail.com>
2016-09-13 21:47:37 +0200

    Dump theme brushes.

---
+++ ResourceKeyBox.Tests/SystemColorsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace ResourceKeyBox.Tests
+{
+    using System;
+    using System.Text;
+
+    using NUnit.Framework;
+
+    using ResourceKeysBox;
+
+    public class SystemColorsTests
+    {
+        [Test]
+        public void DumpBrushDictionary()
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var resource in KeyAndColorResources.SystemColors())
+            {
+                stringBuilder.AppendLine($"<SolidColorBrush x:Key=\"{{x:Static SystemColors.{resource.Keys.BrushKey}Key}}\" Color=\"{{DynamicResource {{x:Static SystemColors.{resource.Keys.ColorKey}Key}}}}\" />");
+            }
+
+            var xaml = stringBuilder.ToString();
+            Console.Write(xaml);
+        }
+    }
+}
---
+++ ResourceKeysBox/ThemeBrushesView.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace ResourceKeysBox
+{
+    using System.Text;
+    using System.Windows.Controls;
+
+    public partial class ThemeBrushesView : UserControl
+    {
+        public ThemeBrushesView()
+        {
+            this.InitializeComponent();
+            var stringBuilder = new StringBuilder();
+
+            foreach (var resource in KeyAndColorResources.SystemColors())
+            {
+                stringBuilder.AppendLine($"<SolidColorBrush x:Key=\"{{x:Static SystemColors.{resource.Keys.BrushKey}Key}}\" Color=\"{{DynamicResource {{x:Static SystemColors.{resource.Keys.ColorKey}Key}}}}\" />");
+            }
+
+            this.XamlBox.Text = stringBuilder.ToString();
+        }
+    }
+}
